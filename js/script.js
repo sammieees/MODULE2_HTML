463c783eb9d1410267805cf50fe6973d3048c770
sammieees <bogosamanthanicole25@gmail.com>
2026-03-09 23:53:49 +0800

    Force update: Personal CV fully functional

---
+++ js/script.js
@@ -0,0 +1,32 @@
+console.log("js/script.js")
+
+const submitBtn = document.getElementById("submitBtn");
+submitBtn.addEventListener("click",function(event){
+    event.preventDefault();
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    if (name===""||email===""){
+    alert("Please fill in all required fields.");
+    } else{
+    alert("Form submitted successfully!");
+    }
+});
+
+const themeToggle = document.getElementById("themeToggle");
+themeToggle.addEventListener("click",function(){
+document.body.classList.toggle("dark-mode");
+    if
+        (document.body.classList.contains("dark-mode")) { themeToggle.textContent = "Toggle Light Mode";
+    }
+    else 
+        { themeToggle.textContent = "Toggle Dark Mode";
+        
+    }
+});
+
+const toggleSkills = document.getElementById("toggleSkills");
+const skillsList = document.getElementById("skillsList");
+
+toggleSkills.addEventListener("click", function(){
+  skillsList.classList.toggle("hidden");
+});
