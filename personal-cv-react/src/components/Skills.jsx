4e5ec6e89e719ab5aaa26638a41af725caed099f
sammieees <bogosamanthanicole25@gmail.com>
2026-03-10 02:09:23 +0800

    WSAT-M4: JavaScript interactivity added

--- personal-cv-react/src/components/Skills.jsx
+++ personal-cv-react/src/components/Skills.jsx
@@ -1,15 +1,22 @@
 import { useState } from "react";
-import Card from "./Card"; 
 
 function Skills({ skills }) {
+  const [visible, setVisible] = useState(true); // true = show, false = hide
+
   return (
-    <Card title="Skills">
-      <ul>
-        {skills.map((skill, index) => (
-          <li key={index}>{skill}</li>
-        ))}
-      </ul>
-    </Card>
+    <section className="card">
+      <h2>Skills</h2>
+      <button onClick={() => setVisible(!visible)}>
+        {visible ? "Hide Skills" : "Show Skills"}
+      </button>
+      {visible && (
+        <ul>
+          {skills.map((skill, index) => (
+            <li key={index}>{skill}</li>
+          ))}
+        </ul>
+      )}
+    </section>
   );
 }
 
