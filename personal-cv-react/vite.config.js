6730cfa2b8e72a6ae7e80d931146e756bbc240e2
sammieees <bogosamanthanicole25@gmail.com>
2026-03-25 16:09:15 +0800

    WSAT-M6: Enhanced React CV with props and dynamic rendering

--- personal-cv-react/vite.config.js
+++ personal-cv-react/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/MODULE2_HTML/',
   plugins: [react()],
-  base: "/MODULE2_HTML/",  
 })
