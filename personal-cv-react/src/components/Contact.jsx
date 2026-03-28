583a6ee9c915811217756ce99a20ebcb4a398ff4
sammieees <bogosamanthanicole25@gmail.com>
2026-03-28 20:57:24 +0800

    WSAT-M7: Connected React to PHP API

--- personal-cv-react/src/components/Contact.jsx
+++ personal-cv-react/src/components/Contact.jsx
@@ -1,30 +1,35 @@
 import { useState } from "react";
+
 function Contact() {
-const [name, setName] = useState("");
-function handleSubmit(e) {
-e.preventDefault();
-fetch("http://localhost/cv-api/process.php", {
-method: "POST",
-headers: {
-"Content-Type": "application/json"
-},
-body: JSON.stringify({ name: name })
-})
-.then(res => res.json())
-.then(data => {
-alert(data.message);
-});
+  const [name, setName] = useState("");
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("Clicked");
+
+    fetch("http://localhost/cv-api/process.php", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ name: name })
+    })
+    .then(res => res.json())
+    .then(data => {
+      alert(data.message);
+    });
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder="Name"
+      />
+      <button type="submit">Send</button>
+    </form>
+  );
 }
-return (
-<form onSubmit={handleSubmit}>
-<input
-type="text"
-value={name}
-onChange={(e) => setName(e.target.value)}
-placeholder="Name"
-/>
-<button type="submit">Send</button>
-</form>
-);
-}
+
 export default Contact;
