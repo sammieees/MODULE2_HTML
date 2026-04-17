9e26b27cfa02011151ed330d423e5676fd19602b
Samantha Nicole Bogo <bogosamanthanicole25@gmail.com>
2026-04-18 00:03:42 +0800

    WSAT-M8: Connected PHP API to MySQL Database

--- getContacts.php
+++ getContacts.php
@@ -1,33 +1,10 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json");
-
 $conn = new mysqli("localhost", "root", "", "cv_database");
-
-if ($conn->connect_error) {
-    echo json_encode([
-        "error" => "Connection failed: " . $conn->connect_error
-    ]);
-    exit();
-}
-
 $sql = "SELECT * FROM contacts ORDER BY id DESC";
 $result = $conn->query($sql);
-
-if (!$result) {
-    echo json_encode([
-        "error" => "Query failed: " . $conn->error
-    ]);
-    exit();
-}
-
 $contacts = [];
-
 while ($row = $result->fetch_assoc()) {
-    $contacts[] = $row;
+$contacts[] = $row;
 }
-
 echo json_encode($contacts);
-
-$conn->close();
 ?>
--- process.php
+++ process.php
@@ -5,45 +5,22 @@ header("Access-Control-Allow-Headers: Content-Type");
 header("Content-Type: application/json");
 
 $conn = new mysqli("localhost", "root", "", "cv_database");
-
-if ($conn->connect_error) {
-    echo json_encode([
-        "message" => "Connection failed: " . $conn->connect_error
-    ]);
-    exit();
-}
-
 $data = json_decode(file_get_contents("php://input"), true);
-
-if (json_last_error() !== JSON_ERROR_NONE) {
-    echo json_encode([
-        "message" => "Invalid JSON input"
-    ]);
-    exit();
-}
-
 if (empty($data['name'])) {
-    echo json_encode([
-        "message" => "Name is required"
-    ]);
-    exit();
+echo json_encode([
+"message" => "Name is required"
+]);
+exit();
 }
-
-
 $name = $conn->real_escape_string($data['name']);
-
 $sql = "INSERT INTO contacts (name) VALUES ('$name')";
-
-
 if ($conn->query($sql) === TRUE) {
-    echo json_encode([
-        "message" => "Data saved successfully"
-    ]);
+echo json_encode([
+"message" => "Data saved successfully"
+]);
 } else {
-    echo json_encode([
-        "message" => "Error saving data: " . $conn->error
-    ]);
+echo json_encode([
+"message" => "Error saving data"
+]);
 }
-
-$conn->close();
 ?>
