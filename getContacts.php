e8e4f1b8260880bb176c97a5fbe660d8a2d568f6
Samantha Nicole Bogo <bogosamanthanicole25@gmail.com>
2026-04-17 23:51:01 +0800

    WSAT-M7: Connected React to PHP API

--- getContacts.php
+++ getContacts.php
@@ -1,10 +1,33 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json");
+
 $conn = new mysqli("localhost", "root", "", "cv_database");
+
+if ($conn->connect_error) {
+    echo json_encode([
+        "error" => "Connection failed: " . $conn->connect_error
+    ]);
+    exit();
+}
+
 $sql = "SELECT * FROM contacts ORDER BY id DESC";
 $result = $conn->query($sql);
-$contacts = [];
-while ($row = $result->fetch_assoc()) {
-$contacts[] = $row;
+
+if (!$result) {
+    echo json_encode([
+        "error" => "Query failed: " . $conn->error
+    ]);
+    exit();
 }
+
+$contacts = [];
+
+while ($row = $result->fetch_assoc()) {
+    $contacts[] = $row;
+}
+
 echo json_encode($contacts);
+
+$conn->close();
 ?>
